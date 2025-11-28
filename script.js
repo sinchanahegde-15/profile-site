149e20a1e77b4b064c166db70f745a9c6892f137
sinchanahegde-15 <sinchanachandrashekar15@gmail.com>
2025-11-28 12:59:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("nav a").hover(
+        function() { $(this).css("color", "#ffcc00"); },
+        function() { $(this).css("color", "#ffffff"); }
+    );
+});
+
+
+
